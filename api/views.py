411431b184be58ddfe2e84bfd64c42517a91e440
BuggyCody <31753974+ThanHtunAung-THA@users.noreply.github.com>
2024-07-14 07:59:30 +0630

    added rest frameworks
    -declare rest frameworks in settings
    -views and urls for custom tokens
    -pip installed new modules for rest frameworks
    -django-cors-headers
    -djangorestframework
    -djangorestframework-simplejwt

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
 # Create your views here.
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+  def validate(self, attrs):
+    data = super().validate(attrs)
+
+    return data
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+  serializer_class = CustomTokenObtainPairSerializer
--- todoapp_api/urls.py
+++ todoapp_api/urls.py
@@ -17,8 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from api.views import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+# ===================== urls =========================== #
 urlpatterns = [
+
+    path('api/login/', CustomTokenObtainPairView.as_view(), name = 'token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
+
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    
+
 ]
