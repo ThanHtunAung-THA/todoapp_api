ece1e73e87f07f1c3639c246fd3236c2f740b413
BuggyCody <31753974+ThanHtunAung-THA@users.noreply.github.com>
2024-07-14 06:56:05 +0630

    added category model
    -created category model
    -declared in admin.py
    -urls of api connected to project's urls

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category (models.Model):
+  name = models.CharField (max_length = 200)
+  note = models.TextField()
+  created_at = models.DateTimeField(auto_now_add = True)
+
+  def __str__(self):
+    return self.name
+
+
+
